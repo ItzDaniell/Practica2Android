46591a4d97d621219b98da56d2cefc6aa7bd9e24
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-05-20 20:41:05 -0500

    Agenda Class

--- app/src/main/java/com/rodriguez/juan/laboratoriocalificado02/Agenda.kt
+++ app/src/main/java/com/rodriguez/juan/laboratoriocalificado02/Agenda.kt
@@ -1,2 +1,35 @@
 package com.rodriguez.juan.laboratoriocalificado02
 
+class Agenda {
+    private val contactos = mutableListOf<Contacto>()
+
+    fun agregarContacto(contacto: Contacto) {
+        contactos.add(contacto)
+        println("Contacto agregado: ${contacto.nombre}")
+    }
+
+    fun listarContactos() {
+        if (contactos.isEmpty()) {
+            println("No hay contactos en la agenda.")
+        } else {
+            println("Lista de contactos:")
+            contactos.forEach { contacto ->
+                println("Nombre: ${contacto.nombre}, Teléfono: ${contacto.telefono}, Email: ${contacto.email}")
+            }
+        }
+    }
+
+    fun buscarPorNombre(nombre: String) {
+        val list = contactos.filter { contacto ->
+            contacto.nombre.contains(nombre, ignoreCase = true)
+        }
+        if (list.isEmpty()) {
+            println("No se encontraron contactos con el nombre \"$nombre\".")
+        } else {
+            println("Resultados de búsqueda:")
+            for (contacto in list) {
+                println("Nombre: ${contacto.nombre}, Teléfono: ${contacto.telefono}, Email: ${contacto.email}")
+            }
+        }
+    }
+}
