bc81f68875766eb105dd8748defaacf3908ffc2a
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-05-20 20:28:04 -0500

    Contacto Class

---
+++ app/src/main/java/com/rodriguez/juan/laboratoriocalificado02/Agenda.kt
@@ -0,0 +1,2 @@
+package com.rodriguez.juan.laboratoriocalificado02
+
---
+++ app/src/main/java/com/rodriguez/juan/laboratoriocalificado02/Contacto.kt
@@ -0,0 +1,7 @@
+package com.rodriguez.juan.laboratoriocalificado02
+
+data class Contacto(
+    val nombre: String,
+    val telefono: String,
+    val email: String
+)
