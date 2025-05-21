697686195e042815361cfca2854ce03b76514b56
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-05-20 20:50:09 -0500

    Ejercicio1Test

---
+++ app/src/test/java/com/rodriguez/juan/laboratoriocalificado02/Ejercicio1Test.kt
@@ -0,0 +1,18 @@
+package com.rodriguez.juan.laboratoriocalificado02
+
+import org.junit.Test
+
+class Ejercicio1Test{
+
+    @Test
+    fun main() {
+        val agenda = Agenda()
+
+        agenda.agregarContacto(Contacto("Juan Daniel", "123456789", "juandaniel@gmail.com"))
+        agenda.agregarContacto(Contacto("Carlos Aguirre", "987654321", "carlosaguirre@gmail.com"))
+
+        agenda.listarContactos()
+
+        agenda.buscarPorNombre("Juan")
+    }
+}
